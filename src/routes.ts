921400a62f786508498bc25f4ac93e4a82abed3b
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2025-09-25 23:05:39 -0300

    Add user authentication and details retrieval functionality

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,9 @@
 import {Router} from 'express'
 import { CreateUserController  } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController'
+import { DatailsUserController } from './controllers/user/DatailsUserController'
+
+import { isAuthenticated } from './middlewares/isAuthenticated'
 
 const router = Router()
 
@@ -8,5 +11,7 @@ const router = Router()
 router.post('/users', new CreateUserController().handle)
 
 router.post('/session', new AuthUserController().handle)
+
+router.get('/me',isAuthenticated, new DatailsUserController().handle)
 
 export {router}
