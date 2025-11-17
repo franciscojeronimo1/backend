1c6f63b50633724dca18cac8a2772fb53c612669
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2025-11-16 22:23:21 -0300

    feat: add environment configuration and Vercel deployment setup
    - Created a new .env file for environment variables including database URL, JWT secret, and Cloudinary credentials.
    - Updated package.json to include new Prisma build scripts for Vercel deployment.
    - Added vercel.json for Vercel configuration to handle serverless deployment.
    - Modified Prisma client import in index.ts to use the official package.
    - Updated server.ts to listen on the PORT defined in the environment variables.

--- src/server.ts
+++ src/server.ts
@@ -31,4 +31,4 @@ app.use((err: Error, req:Request, res:Response, next:NextFunction)=> {
     })
 })
 
-app.listen(3333, ()=> console.log('servidor online!!'))
+app.listen(process.env.PORT, ()=> console.log('servidor online!!'))
