880c01c8bca4a7187f5a16681c9c79546af257c0
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2025-11-20 10:32:40 -0300

    feat: enhance Prisma client initialization with error handling
    - Configured PrismaClient to log queries, errors, and warnings in development mode, and only errors in production.
    - Added error handling during database connection to log any connection issues.

--- dist/prisma/index.js
+++ dist/prisma/index.js
@@ -1,5 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
-const prismaClient = new client_1.PrismaClient();
+const prismaClient = new client_1.PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+});
+// Tratamento de erro na inicialização
+prismaClient.$connect().catch((error) => {
+    console.error('Erro ao conectar com o banco de dados:', error);
+});
 exports.default = prismaClient;
